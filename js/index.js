f51ef0b5048def09f5767ee441325786257c66a3
Anastasiya1301 <dudkoanastasiya1301@gmail.com>
2025-06-02 20:28:17 +0300

    js4

---
+++ js/index.js
@@ -0,0 +1,60 @@
+//1
+
+const text1 = prompt("Введіть число");
+const text2 = prompt("Введіть число");
+
+if(text1 !== "" && text2 !== ""){
+    alert("Обидва поля заповнені");
+} else {
+    alert("Не всі поля заповнені");
+}
+
+
+//2
+
+const num1 = prompt("Введіть число");
+const num2 = prompt("Введіть число");
+const result = num1 + num2;
+
+if (result > 10) {
+    alert("Сума більша за 10");
+} else {
+    alert("Сума менша або дорівнює 10");
+}
+
+
+//3
+
+const textJava = prompt("Введіть текст");
+
+if (textJava === "JavaScript"){
+    alert("Текст містить слово JavaScript");
+} else {
+    alert("Текст не містить слово JavaScript");
+}
+
+
+
+//4
+
+const saveNumber = prompt("Введіть число");
+
+if (saveNumber >= 10 && saveNumber <= 20) {
+    alert("Число входить в діапазон від 10 до 20");
+} else {
+    alert("Число не входить в діапазон від 10 до 20");
+}
+
+
+
+//5
+
+let number = prompt("Введіть свій номер телефону");
+let email = prompt("Введіть свою електронну адресу");
+let password = prompt("Введіть свій пароль");
+
+if (number && email && password) {
+    alert("Перенаправлення на іншу сторінку");
+} else {
+    alert("Помилка: неправильне заповнення");
+}
